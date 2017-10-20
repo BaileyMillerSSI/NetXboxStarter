a4c8a27ad5e28ec980a60469ef9df5a26e6a4eab
Bailey Miller <bmiller@susquehannasoftware.com>
2017-10-20 01:36:23 -0400

    Improvements for async methods.

--- XboxOn/XboxOn/XboxOn.cs
+++ XboxOn/XboxOn/XboxOn.cs
@@ -4,16 +4,19 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace XboxOn
 {
     public static class XboxOn
     {
+        public static int SendAttempts { get; set; } = 5;
 
-        public static String IpAddress { get; private set; }
+        public static String IpAddress { get; private set; } = "";
         public static int Port { get; private set; } = 5050;
 
-        public static String LiveId { get; private set; }
+        public static String LiveId { get; private set; } = "";
 
         public static IPEndPoint Endpoint
         {
@@ -27,9 +30,6 @@ namespace XboxOn
         {
             var XBOX_POWER = "dd02001300000010";
 
-
-            Console.WriteLine("Sending Power on Packet....");
-
             var endPoint = new IPEndPoint(IPAddress.Any, 0);
 
 
@@ -42,14 +42,30 @@ namespace XboxOn
             var power_packet = StringToByteArray(XBOX_POWER).Concat(Encoding.UTF8.GetBytes(LiveId + "\x00"));
 
 
-            for (int x = 0; x < 5; x++)
+            for (int x = 0; x < SendAttempts; x++)
             {
                 s.Send(power_packet.ToArray());
-                System.Threading.Thread.Sleep(100);
+                Thread.Sleep(TimeSpan.FromSeconds(.5));
             }
 
 
         }
+
+        public static async void PowerOnXboxOneAsync()
+        {
+            await Task.Run(()=> 
+            {
+                PowerOnXboxOne();
+            });
+        }
+
+        public static void ImportSettings(String Ip, String LiveId, int Port = 5050)
+        {
+            IpAddress = Ip;
+            XboxOn.LiveId = LiveId;
+            XboxOn.Port = Port;
+        }
+
         private static byte[] StringToByteArray(String hex)
         {
             int NumberChars = hex.Length;
